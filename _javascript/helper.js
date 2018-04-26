f5cda8352e94aca94f223aabe114889dcde70f2e
Vikanda Gonzales <vikanda.gonzales@yahoo.com>
2018-04-26 11:50:09 -0700

    made posts dynamically populate

---
+++ _javascript/helper.js
@@ -0,0 +1,13 @@
+function empty (element) {
+  while (element.firstChild) {
+    element.removeChild(element.firstChild);
+  }
+  return element;
+}
+
+function addClassesToElement (element, ...classes) {
+  return classes.reduce((acc, ele) => {
+    element.classList.add(ele);
+    return acc;
+  }, element);
+}
