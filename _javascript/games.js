8757029c6fa54b5b3e7ec10946dbedf3699003c4
Vikanda Gonzales <vikanda.gonzales@yahoo.com>
2018-04-27 05:01:07 -0700

    added members and game pages

--- _javascript/games.js
+++ _javascript/games.js
@@ -1,61 +1,5 @@
 const games = document.querySelector('#games');
 
-function gameLeft (game) {
-
-    const section = document.createElement('section');
-    addClassesToElement(section, 'hero', 'is-medium', 'is-dark', 'game', 'game-left');
-    section.style.background = `linear-gradient(to right, rgba(0,0,0, .9), rgba(0,0,0, .2)), url(${game.image}) center/cover`;
-
-      const overlay = document.createElement('div');
-      addClassesToElement(overlay, 'game-bg');
-
-      const body = document.createElement('div');
-      addClassesToElement(body, 'hero-body');
-
-        const container = document.createElement('div');
-        addClassesToElement(container, 'container');
-
-          const title = document.createElement('h1');
-          addClassesToElement(title, 'title', 'is-2');
-          title.innerHTML = game.name;
-          container.appendChild(title);
-
-        body.appendChild(container);
-
-      section.appendChild(overlay);
-      section.appendChild(body);
-
-    games.appendChild(section);
-}
-
-function gameRight (game) {
-
-    const section = document.createElement('section');
-    addClassesToElement(section, 'hero', 'is-medium', 'is-dark', 'game', 'game-right');
-    section.style.background = `linear-gradient(to left, rgba(0,0,0, .9), rgba(0,0,0, .2)), url(${game.image}) center/cover`;
-
-      const overlay = document.createElement('div');
-      addClassesToElement(overlay, 'game-bg');
-
-      const body = document.createElement('div');
-      addClassesToElement(body, 'hero-body');
-
-        const container = document.createElement('div');
-        addClassesToElement(container, 'container');
-
-          const title = document.createElement('h1');
-          addClassesToElement(title, 'title', 'is-2');
-          title.innerHTML = game.name;
-          container.appendChild(title);
-
-        body.appendChild(container);
-
-      section.appendChild(overlay);
-      section.appendChild(body);
-
-    games.appendChild(section);
-}
-
 axios.get('http://localhost:3000/games')
   .then(response => {
     const games = response.data.data
@@ -67,3 +11,67 @@ axios.get('http://localhost:3000/games')
       }
     })
   });
+
+function gameLeft (game) {
+  const section = document.createElement('section');
+  addClassesToElement(section, 'hero', 'is-medium', 'is-dark', 'game', 'game-left', 'pointer');
+  section.style.background = `linear-gradient(to right, rgba(0,0,0, .9), rgba(0,0,0, .2)), url(${game.image}) center/cover`;
+  section.addEventListener('click', event => {
+    goToGame(game);
+  });
+
+    const overlay = document.createElement('div');
+    addClassesToElement(overlay, 'game-bg');
+
+    const body = document.createElement('div');
+    addClassesToElement(body, 'hero-body');
+
+      const container = document.createElement('div');
+      addClassesToElement(container, 'container');
+
+        const title = document.createElement('h1');
+        addClassesToElement(title, 'title', 'is-2');
+        title.innerHTML = game.name;
+        container.appendChild(title);
+
+      body.appendChild(container);
+
+    section.appendChild(overlay);
+    section.appendChild(body);
+
+  games.appendChild(section);
+}
+
+function gameRight (game) {
+  const section = document.createElement('section');
+  addClassesToElement(section, 'hero', 'is-medium', 'is-dark', 'game', 'game-right', 'pointer');
+  section.style.background = `linear-gradient(to left, rgba(0,0,0, .9), rgba(0,0,0, .2)), url(${game.image}) center/cover`;
+  section.addEventListener('click', event => {
+    goToGame(game);
+  });
+
+    const overlay = document.createElement('div');
+    addClassesToElement(overlay, 'game-bg');
+
+    const body = document.createElement('div');
+    addClassesToElement(body, 'hero-body');
+
+      const container = document.createElement('div');
+      addClassesToElement(container, 'container');
+
+        const title = document.createElement('h1');
+        addClassesToElement(title, 'title', 'is-2');
+        title.innerHTML = game.name;
+        container.appendChild(title);
+
+      body.appendChild(container);
+
+    section.appendChild(overlay);
+    section.appendChild(body);
+
+  games.appendChild(section);
+}
+
+function goToGame (game) {
+  window.location = `/game.html?id=${game.id}`;
+}
--- _javascript/helper.js
+++ _javascript/helper.js
@@ -23,3 +23,23 @@ function getQueryVariable (variable) {
     }
   return false;
 }
+
+
+function renderMembers (members) {
+
+  members.forEach(member => {
+    const div = document.createElement('div');
+    addClassesToElement(div, 'member', 'pointer');
+
+      const img = document.createElement('img');
+      img.src = member.image;
+      div.appendChild(img);
+
+      const name = document.createElement('div');
+      addClassesToElement(name, 'member-label');
+      name.innerHTML = member.fname;
+      div.appendChild(name);
+
+    memberContainer.appendChild(div);
+  });
+}
